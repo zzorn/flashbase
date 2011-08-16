5880e3a97db5f062e81cc856ba1f665acfc58894
zzorn <zzorn@iki.fi>
2011-08-16 17:10:05 +0300

    Remove old build result before build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+rm target/TestGame.swf
+
 ~/frameworks/flex/bin/mxmlc --target-player=10.2.0 -optimize=true -incremental=true -output target/TestGame.swf -static-link-runtime-shared-libraries=true -compiler.debug=true -compatibility-version=3.0.0  src/TestGame.as
 
 cp src/*.html target/
